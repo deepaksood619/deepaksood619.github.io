c7238f04bfe699f3a59508d2110a96b787636507
Deepak <deepaksood619@gmail.com>
2024-03-07 22:50:22 +0530

    updated docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -130,6 +130,35 @@ const config = {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'X3OY8NGHVH',
+  
+        // Public API key: it is safe to commit it
+        apiKey: '55231a2d373a93253025c5b165065e15',
+  
+        indexName: 'deepaksood619io',
+  
+        // Optional: see doc section below
+        contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
+      },
     }),
     plugins: [
       [
